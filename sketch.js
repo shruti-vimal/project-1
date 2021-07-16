c156090b11d4fd557a53e2f22d0769f58fa44609
shruti-vimal <72246905+shruti-vimal@users.noreply.github.com>
2021-07-16 16:06:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup(){
+    createCanvas(1200,600);
+    
+         a=createSprite(50,50,20,20);
+        a.shapeColor="yellow";
+    }
+
+
+function draw(){
+    background("white");
+    drawSprites();
+
+}
